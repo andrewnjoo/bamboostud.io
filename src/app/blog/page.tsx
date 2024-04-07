5eb762d82efba0e9c590485b607fac6574a28ebd
Andrew Njoo <adnjoo@gmail.com>
2024-04-07 11:30:18 -0700

    copy

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -12,7 +12,7 @@ export default async function Blog() {
             From the blog
           </h2>
           <p className='mt-2 text-lg leading-8 text-gray-600'>
-            Learn how to better manage your finances!
+            Learn how to grow your business with these blog posts.
           </p>
         </div>
         <div className='mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
