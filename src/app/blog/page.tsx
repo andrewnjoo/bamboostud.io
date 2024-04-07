b143e8447c1a01b080f8f2cf0c378087ee4e3f27
Andrew Njoo <adnjoo@gmail.com>
2024-04-07 11:29:25 -0700

    page height

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ export default async function Blog() {
   const posts = await getPosts();
 
   return (
-    <div className='bg-white py-24 sm:py-32'>
+    <div className='bg-white py-24 sm:py-32 min-h-[80vh]'>
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
         <div className='mx-auto max-w-2xl lg:mx-0'>
           <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
